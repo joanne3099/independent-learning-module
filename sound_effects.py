44bb45a55ca17aa215e7833f250d0bb8a3dd63ac
joanne <78866701+joanne3099@users.noreply.github.com>
2021-05-20 14:11:14 +0100

    Update sound_effects.py

--- sound_effects.py
+++ sound_effects.py
@@ -1,7 +1,9 @@
 import pygame
 
+# Initializes the sound mixer to be used 
+
 pygame.mixer.init()
 
-
+# Loads the sounds into the game 
 bullet_sound = pygame.mixer.Sound('sounds/laser10.wav')
 alien_sound = pygame.mixer.Sound('sounds/explosion.wav')
